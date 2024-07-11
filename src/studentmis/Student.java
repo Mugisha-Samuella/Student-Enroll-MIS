a9a0abd3d047e38720191854640a3bc14493b5b4
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-11 23:02:54 +0300

    Initial commit

---
+++ src/studentmis/Student.java
@@ -0,0 +1,26 @@
+package studentmis;
+
+public class Student {
+
+    private String firstname;
+    private String lastname;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+
+
+    //Constructor: prompt user to enter student's name and year
+
+    //Generate an ID
+
+    //Enroll in courses
+
+    //View Balance
+
+    //Pay tuition
+
+    //show status
+}
---
+++ src/studentmis/StudentDatabaseApp.java
@@ -0,0 +1,9 @@
+package studentmis;
+
+public class StudentDatabaseApp {
+
+    //Ask how many new users we want to add
+
+    //Create n number of new students
+
+}
