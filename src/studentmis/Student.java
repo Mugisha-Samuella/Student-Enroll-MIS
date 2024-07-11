a3ab3d829655046f7d2a4a9531f808bb5de05acc
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-07-11 23:33:57 +0300

    prompt user to enter student's name, year and print the output

--- src/studentmis/Student.java
+++ src/studentmis/Student.java
@@ -1,10 +1,12 @@
 package studentmis;
 
+import java.util.Scanner;
+
 public class Student {
 
     private String firstname;
     private String lastname;
-    private String gradeYear;
+    private int gradeYear;
     private int studentID;
     private String courses;
     private int tuitionBalance;
@@ -13,6 +15,21 @@ public class Student {
 
 
     //Constructor: prompt user to enter student's name and year
+
+    public Student(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter First Name: ");
+        this.firstname = in.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        this.lastname = in.nextLine();
+
+        System.out.println("1 - Sophomore\n 2 - Freshman\n 3 - Junior\n 4 - Senior\n Enter grade level: ");
+        this.gradeYear = in.nextInt();
+
+        System.out.println(firstname + " " + lastname + ": " + gradeYear);
+    }
 
     //Generate an ID
 
--- src/studentmis/StudentDatabaseApp.java
+++ src/studentmis/StudentDatabaseApp.java
@@ -2,8 +2,12 @@ package studentmis;
 
 public class StudentDatabaseApp {
 
-    //Ask how many new users we want to add
+    public static void main(String[] args) {
+        Student student1 = new Student();
 
-    //Create n number of new students
+        //Ask how many new users we want to add
+
+        //Create n number of new students
+    }
 
 }
